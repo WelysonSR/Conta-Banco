cac8c005afc12a3a86a4d1f5c3d756f422de0e13
Welyson Rosa <welysonsr@gmail.com>
2023-09-20 16:45:31 -0300

    feat: Desafio DIO - Trilha Java Básico

---
+++ src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Por favor, digite o nome do cliente!");
+            String nomeCliente = scanner.nextLine();
+
+            System.out.println("Por favor, digite o número da conta!");
+            int numero = scanner.nextInt();
+
+            System.out.println("Por favor, digite o número da Agência !");
+            String agencia = scanner.next();
+
+            System.out.println("Por favor, digite o saldo da conta!");
+            double saldo = scanner.nextDouble();
+
+            scanner.close();
+
+            System.out.println(
+                    "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                            ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+            throw e;
+        }
+    }
+}
